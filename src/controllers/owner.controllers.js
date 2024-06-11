59a790d19108fc5c80d11cf8112f1ac5533d5638
Yaseen <hackercanhack69@gmail.com>
2024-06-11 23:21:27 +0530

    added owner controller and utility files

---
+++ src/controllers/owner.controllers.js
@@ -0,0 +1,21 @@
+import {ownerModel} from "../models/owner.models.js"
+import { ApiResponse } from "../utils/apiResponse.js"
+import { ApiError } from "../utils/apiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const createOwner = asyncHandler(async(req,res)=>{
+    const {fullName,email,password} = req.body;
+    const currOwners = ownerModel.find();
+    if(currOwners.length>0){
+        throw new ApiError(401,"You are unauthorized");
+    }
+    const owner = await ownerModel.create({fullName,email,password});
+    if(!owner){
+        throw new ApiError(400,"Something went wrong!");
+    }
+    res.send(200).json(new ApiResponse(owner,"Owner created successfully!"));
+
+})
+
+
+export {createOwner}
